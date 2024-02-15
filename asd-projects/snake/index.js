c0386f0c2f533d78f7f228e1a1885c24e795d1bb
c-eid <eid486@stcharles.k12.la.us>
2024-02-14 19:27:24 -0600

    Added snake basic

--- asd-projects/snake/index.js
+++ asd-projects/snake/index.js
@@ -11,7 +11,10 @@ var highScoreElement = $("#highScore");
 
 // TODO 4a: Create the snake, apple and score variables
 // Game Variables
-
+var snake = {
+};
+var apple = {
+};
 // Constant Variables
 var ROWS = 20;
 var COLUMNS = 20;
@@ -41,11 +44,18 @@ init();
 
 function init() {
   // TODO 4c-2: initialize the snake
+  // initialize the snake's body as an empty Array
+  snake.body = [];
+
+  // make the first snakeSquare and set it as the head
+  makeSnakeSquare(10, 10);
+  snake.head = snake.body[0];
 
   // TODO 4b-2: initialize the apple
-
+  makeApple();
   // TODO 5a: Initialize the interval
-
+  // start update interval
+  updateInterval = setInterval(update, 100);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -58,6 +68,15 @@ function init() {
  */
 function update() {
   // TODO 5b: Fill in the update function's code block
+   moveSnake();
+  
+    if (hasHitWall() || hasCollidedWithSnake()) {
+      endGame();
+    }
+  
+    if (hasCollidedWithApple()) {
+      handleAppleCollision();
+    }
 }
 
 function checkForNewDirection(event) {
@@ -65,14 +84,34 @@ function checkForNewDirection(event) {
   TODO 6b: Update snake.head.direction based on the value of activeKey.
   
   BONUS: Only allow direction changes to take place if the new direction is
-  perpendicular to the current direction
+  perpendicular to the current direction --> did that
   */
 
   if (activeKey === KEY.LEFT) {
-    snake.head.direction = "left";
+
+    if(snake.head.direction !== "right"){
+      snake.head.direction = "left";
+    }
+
+  }
+  else if (activeKey === KEY.RIGHT) {
+    
+    if(snake.head.direction !== "left"){
+      snake.head.direction = "right";
+    }
+  }
+  else if (activeKey === KEY.UP) {
+    if(snake.head.direction !== "down"){
+      snake.head.direction = "up";
+    }
+    
+  } else if (activeKey === KEY.DOWN) {
+    if(snake.head.direction !== "up"){
+      snake.head.direction = "down";
+    }
   }
 
-  // FILL IN THE REST
+  // // FILL IN THE REST
 
   // console.log(snake.head.direction);     // uncomment me!
 }
@@ -87,25 +126,53 @@ function moveSnake() {
   column/row properties. 
   
   */
+ 
+  for (var i = snake.body.length-1; i > 0; i--) {
+      var snakeSquare = snake.body[i];
+  
+      var nextSnakeSquare = snake.body[i - 1];
+      var nextRow = nextSnakeSquare.row;
+      var nextColumn = nextSnakeSquare.column;
+      var nextDirection = nextSnakeSquare.direction;
+  
+      snakeSquare.direction = nextDirection;
+      snakeSquare.row = nextRow;
+      snakeSquare.column = nextColumn;
+      repositionSquare(snakeSquare);
+  }
 
   //Before moving the head, check for a new direction from the keyboard input
   checkForNewDirection();
 
   /* 
   TODO 7: determine the next row and column for the snake's head
-  
+
   HINT: The snake's head will need to move forward 1 square based on the value
   of snake.head.direction which may be one of "left", "right", "up", or "down"
   */
+  if (snake.head.direction === "left") {
+    snake.head.column = snake.head.column - 1;
+  } else if (snake.head.direction === "right"){
+    snake.head.column = snake.head.column + 1;
+  }else if (snake.head.direction === "down"){
+    snake.head.row = snake.head.row + 1;
+  }else if (snake.head.direction === "up"){
+    snake.head.row = snake.head.row - 1;
+  }
+  repositionSquare(snake.head);
 }
 
 function hasHitWall() {
   /* 
   TODO 8: Should return true if the snake's head has collided with the four walls of the
   board, false otherwise.
-  
+
   HINT: What will the row and column of the snake's head be if this were the case?
   */
+
+  if(snake.head.column > COLUMNS || snake.head.column < 0 || snake.head.row < 0 || snake.head.row > ROWS){
+    return  true
+  }
 
   return false;
 }
@@ -117,6 +184,9 @@ function hasCollidedWithApple() {
   
   HINT: Both the apple and the snake's head are aware of their own row and column
   */
+  if(apple.row === snake.head.row && apple.column === snake.head.column){
+    return true
+  }
 
   return false;
 }
@@ -139,9 +209,20 @@ function handleAppleCollision() {
   If the tail is moving "down", place the next snakeSquare above it.
   etc...
   */
-  var row = 0;
-  var column = 0;
+  var row = snake.tail.row;
+  var column = snake.tail.column;
 
+  if(snake.tail.direction === "left"){
+    row++
+  } 
+  if (snake.tail.direction === "right"){
+    row--
+  }
+   if (snake.tail.direction === "top"){
+    column--
+  }  if (snake.tail.direction === "bottom"){
+    column++
+  }
   // code to determine the row and column of the snakeSquare to add to the snake
 
   makeSnakeSquare(row, column);
@@ -156,7 +237,11 @@ function hasCollidedWithSnake() {
   head and each part of the snake's body also knows its own row and column.
   
   */
-
+  for(var i = 1; i < snake.body.length; i++){
+    if(snake.head.row === snake.body[i].row && snake.head.column === snake.body[i].column){
+      return true
+    }
+  }
   return false;
 }
 
@@ -185,6 +270,18 @@ function endGame() {
  */
 function makeApple() {
   // TODO 4b-1: Fill in the makeApple() code block
+
+  apple.element = $("<div>").addClass("apple").appendTo(board);
+
+  // get a random available row/column on the board
+  var randomPosition = getRandomAvailablePosition();
+
+  // initialize the row/column properties on the Apple Object
+  apple.row = randomPosition.row;
+  apple.column = randomPosition.column;
+
+  // position the apple on the screen
+  repositionSquare(apple);
 }
 
 /* Create an HTML element for a snakeSquare using jQuery. Then, given a row and
@@ -193,6 +290,27 @@ function makeApple() {
  */
 function makeSnakeSquare(row, column) {
   // TODO 4c-1: Fill in this function's code block
+
+  var snakeSquare = {};
+
+  // make the snakeSquare.element Object and append it to the board
+  snakeSquare.element = $("<div>").addClass("snake").appendTo(board);
+
+  // initialize the row and column properties on the snakeSquare Object
+  snakeSquare.row = row;
+  snakeSquare.column = column;
+
+  // set the position of the snake on the screen
+  repositionSquare(snakeSquare);
+
+  // if this is the head, add the snake-head id
+  if (snake.body.length === 0) {
+    snakeSquare.element.attr("id", "snake-head");
+  }
+
+  // add snakeSquare to the end of the body Array and set it as the new tail
+  snake.body.push(snakeSquare);
+  snake.tail = snakeSquare;
 }
 
 /* 
@@ -208,7 +326,8 @@ function makeSnakeSquare(row, column) {
 */
 function handleKeyDown(event) {
   // TODO 6a: make the handleKeyDown function register which key is pressed
-  
+activeKey = event.which;
+console.log(activeKey);
 }
 
 /* Given a gameSquare (which may be a snakeSquare or the apple), position
@@ -236,7 +355,14 @@ function getRandomAvailablePosition() {
   while (!spaceIsAvailable) {
     randomPosition.column = Math.floor(Math.random() * COLUMNS);
     randomPosition.row = Math.floor(Math.random() * ROWS);
-    spaceIsAvailable = true;
+
+    for(var i = 0; i < snake.body.length; i++){
+      if(snake.body[i].row === randomPosition.row && snake.body[i].column === randomPosition.column){
+        spaceIsAvailable = false
+      }else{
+        spaceIsAvailable = true
+      }
+    }
 
     /*
     TODO 13: After generating the random position determine if that position is
---
+++ projects/3dSnake/snake.js
@@ -0,0 +1,463 @@
+
+
+
+var cubes = [
+    {
+        made: false,
+        x: 1280,
+        y: 1400,
+        offset: 100,
+        radius: 990,
+    },
+    {
+        made: false,
+        x: 400,
+        y: 400,
+        offset: 100,
+        radius: 45,
+        angleOffset: 0,
+        radiusOffset: 45,
+    },
+    {
+        made: false,
+        x: 600,
+        y: 2560,
+        offset: 100,
+        radius: 45,
+        angleOffset: 0,
+        radiusOffset: 135,
+    },    
+    {
+        made: false,
+        x: 600,
+        y: 2560,
+        offset: 100,
+        radius: 45,
+        angleOffset: 0,
+        radiusOffset: 225,
+    },
+    {
+        made: false,
+        x: 600,
+        y: 2560,
+        offset: 100,
+        radius: 45,
+        angleOffset: 0,
+        radiusOffset: 315,
+    },
+    {
+        made: false,
+        x: 600,
+        y: 2560,
+        offset: 100,
+        radius: 45,
+        angleOffset: 0,
+        radiusOffset: 405,
+    },
+    {
+        made: false,
+        x: 600,
+        y: 2560,
+        offset: 100,
+        radius: 45,
+        angleOffset: 0,
+        radiusOffset: 495,
+    },
+    {
+        made: false,
+        x: 600,
+        y: 2560,
+        offset: 100,
+        radius: 45,
+        angleOffset: 0,
+        radiusOffset: 585,
+    },
+    {
+        made: false,
+        x: 600,
+        y: 2560,
+        offset: 100,
+        radius: 45,
+        angleOffset: 0,
+        radiusOffset: 675,
+    },
+
+    {
+        made: false,
+        x: 600,
+        y: 2560,
+        offset: 100,
+        radius: 45,
+        angleOffset: 0,
+        radiusOffset: 765,
+    },
+
+    {
+        made: false,
+        x: 600,
+        y: 2560,
+        offset: 100,
+        radius: 45,
+        angleOffset: 0,
+        radiusOffset: 855,
+    },
+    {
+        made: false,
+        x: 600,
+        y: 2560,
+        offset: 100,
+        radius: 45,
+        angleOffset: 0,
+        radiusOffset: 945,
+    },
+    {
+        made: false,
+        x: 600,
+        y: 2560,
+        offset: 100,
+        radius: 45,
+        angleOffset: 0,
+        radiusOffset: 1035,
+    },
+    {
+        made: false,
+        x: 600,
+        y: 2560,
+        offset: 100,
+        radius: 45,
+        angleOffset: 0,
+        radiusOffset: 1125
+    },
+    {
+        made: false,
+        x: 600,
+        y: 2560,
+        offset: 100,
+        radius: 45,
+        angleOffset: 0,
+        radiusOffset: 1215,
+    },
+    {
+        made: false,
+        x: 600,
+        y: 2560,
+        offset: 100,
+        radius: 45,
+        angleOffset: 0,
+        radiusOffset: 1305,
+    },
+    {
+        made: false,
+        x: 600,
+        y: 2560,
+        offset: 100,
+        radius: 45,
+        angleOffset: 0,
+        radiusOffset: 1395,
+    },
+    {
+        made: false,
+        x: 600,
+        y: 2560,
+        offset: 100,
+        radius: 45,
+        angleOffset: 0,
+        radiusOffset: 1485,
+    },
+    {
+        made: false,
+        x: 600,
+        y: 2560,
+        offset: 100,
+        radius: 45,
+        angleOffset: 0,
+        radiusOffset: 1575,
+    },
+    {
+        made: false,
+        x: 600,
+        y: 2560,
+        offset: 100,
+        radius: 45,
+        angleOffset: 0,
+        radiusOffset: 1665,
+    },{
+        made: false,
+        x: 600,
+        y: 2560,
+        offset: 100,
+        radius: 45,
+        angleOffset: 0,
+        radiusOffset: 1755,
+    },{
+        made: false,
+        x: 600,
+        y: 2560,
+        offset: 100,
+        radius: 45,
+        angleOffset: 0,
+        radiusOffset: 1845,
+    },{
+        made: false,
+        x: 600,
+        y: 2560,
+        offset: 100,
+        radius: 45,
+        angleOffset: 0,
+        radiusOffset: 1935,
+    },
+
+];
+var ran;
+
+var clicked;
+var open;
+var angle1 = 0;
+var angle2 = 0;
+var angle3 = 0;
+var angle4 = 0;
+var mouse = {
+    x: 0,
+    y: 0
+};
+debugger;
+const canvas = document.getElementById("canvas");
+var documentHeight = document.body.clientHeight;
+var documentWidth = document.body.clientWidth;
+canvas.width = documentWidth * 2;
+canvas.height = documentWidth * 2;
+
+$("#canvas").css("width", documentWidth * 2 + "px").css("height", documentWidth * 2 + "px").css("left", (-(documentWidth / 2)) + "px").css("top", (-(documentWidth / 1.35)) + "px");
+
+const ctx = canvas.getContext("2d");
+ctx.lineWidth = 10;
+
+
+// $("#circle").empty()
+// $("#circle2").empty()
+// $("#circle3").empty()
+// $("#circle4").empty()
+// $("#circle5").empty()
+// $("#circle6").empty()
+// $("#circle7").empty()
+// $("#circle8").empty()
+
+var offset = 100;
+var ang = 1;
+var down = 1.5
+var previous = 0
+var backgroundRotation = 0;
+function main() {
+    
+    if (ang > 0.1) {
+        ang -= 0.001;
+    }
+    renderShape();
+
+    angle1 += ang;
+    
+
+
+
+
+    if (angle1 >= 90) {
+        angle2 += ang;
+    }
+    if (angle1 >= 180) {
+        angle3 += ang;
+    }
+    if (angle1 >= 270) {
+        angle4 += ang;
+        ang = 0.1;
+
+        //     if (offset <= 100) {
+        //     offset += 0.01
+        // }
+    }
+    if(clicked){
+        const dataArray = new Uint8Array(bufferLength);
+        analyser.getByteFrequencyData(dataArray);
+        ctx.clearRect(-10000, -10000, 20000, 20000);
+        console.log(dataArray[14])
+        backgroundRotation += 0.01 + dataArray[14]/5000
+        document.getElementById("canvas").style.transform = "rotate(" + backgroundRotation + "deg" + ")";
+        console.log(dataArray[3]);
+        let lWidth = dataArray[23]/15
+        
+        for (var i = 0; i < documentWidth * 2; i += 100 + (previous/10)) {
+            ctx.beginPath();
+            ctx.lineWidth = 0 + lWidth;
+            ctx.strokeStyle = "white";
+            ctx.moveTo(0, i);
+            ctx.lineTo(documentWidth * 2, i);
+            ctx.closePath();
+            ctx.stroke();
+            ctx.beginPath();
+            ctx.lineWidth = 0 + lWidth;
+            ctx.moveTo(i, 0);
+            ctx.lineTo(i, documentWidth * 2);
+            ctx.closePath();
+            ctx.stroke();
+        }
+     
+        if(dataArray[3]>= 245){
+            previous += 5
+        }
+        if (previous > 0){
+        previous -= down;
+        }
+    }
+
+   
+
+}
+
+var id = setInterval(main, (1000 / 240));
+
+
+
+function alongPath(id, angle, xposLocal = 400, yposLocal = 400, radius) {
+    var Y = yposLocal / 2 + ((Math.sin(angle * Math.PI / 180) * radius)) / (1 + (offset / 100));
+    var X = xposLocal + (Math.cos(angle * Math.PI / 180) * radius);
+
+    $(id).css("top", Y + "px")
+        .css("left", X + "px");
+
+    return {
+        point: " " + X + "," + Y + " ",
+        x: X,
+        y: Y
+    };
+}
+
+
+function changeColor(id) {
+    $(id).css("display", "none");
+
+}
+function renderShape() {
+    for (var i = 0; i < cubes.length; i++) {
+        if (cubes[i].made === false) {
+            // for (var j = 1; j <= 8; j++) {
+            //     $("<div>").attr("id", "circle" + j + "" + i)
+            //         .addClass("circle")
+            //         .text(j + "")
+            //         .appendTo(".circlesDiv")
+
+            // }
+            $(document.createElementNS('http://www.w3.org/2000/svg', 'polygon')).attr("id", "front" + i).attr("points", "1,1 1,1 1,1 1,1").appendTo("svg").css("fill", "rgb(122,0,122)").css("stroke", "rgba(0, 0, 0)").css("stroke-width", "3").addClass("polygon" + i);
+            $(document.createElementNS('http://www.w3.org/2000/svg', 'polygon')).attr("id", "back" + i).attr("points", "1,1 1,1 1,1 1,1").appendTo("svg").css("fill", "rgb(122,0,122)").css("stroke", "rgba(0, 0, 0)").css("stroke-width", "3").addClass("polygon" + i);
+            $(document.createElementNS('http://www.w3.org/2000/svg', 'polygon')).attr("id", "left" + i).attr("points", "1,1 1,1 1,1 1,1").appendTo("svg").css("fill", "rgb(122,0,122)").css("stroke", "rgba(0, 0, 0)").css("stroke-width", "3").addClass("polygon" + i);
+            $(document.createElementNS('http://www.w3.org/2000/svg', 'polygon')).attr("id", "top" + i).attr("points", "1,1 1,1 1,1 1,1").appendTo("svg").css("fill", "rgb(122,0,122)").css("stroke", "rgba(0, 0, 0)").css("stroke-width", "3").addClass("polygon" + i);
+            $(document.createElementNS('http://www.w3.org/2000/svg', 'polygon')).attr("id", "bottom" + i).attr("points", "1,1 1,1 1,1 1,1").appendTo("svg").css("fill", "rgb(122,0,122)").css("stroke", "rgba(0, 0, 0)").css("stroke-width", "3").addClass("polygon" + i);
+            $(document.createElementNS('http://www.w3.org/2000/svg', 'polygon')).attr("id", "right" + i).attr("points", "1,1 1,1 1,1 1,1").appendTo("svg").css("fill", "rgb(122,0,122)").css("stroke", "rgba(0, 0, 0)").css("stroke-width", "3").addClass("polygon" + i);
+
+            cubes[i].made = true;
+        }
+
+        $(".polygon" + i).css("stroke", "rgb(0,0,0)").css("fill", "rgb(0,255,255)").css("stroke-width", "1");
+        $(".polygon" + 0).css("stroke", "rgb(0,0,0)").css("fill", "rgb(255,255,255)").css("stroke-width", "1");
+
+
+        var cubesY = alongPath("#circle1" + i, angle1, cubes[i].x, cubes[i].y, cubes[i].radius);
+        alongPath("#circle5" + i, angle1, cubes[i].x, cubes[i].y + cubes[i].offset, cubes[i].radius);
+        if (angle1 >= 90) {
+
+            alongPath("#circle2" + i, angle2, cubes[i].x, cubes[i].y, cubes[i].radius);
+            alongPath("#circle6" + i, angle2, cubes[i].x, cubes[i].y + cubes[i].offset, cubes[i].radius);
+        }
+        if (angle1 >= 180) {
+
+            alongPath("#circle3" + i, angle3, cubes[i].x, cubes[i].y, cubes[i].radius);
+            alongPath("#circle7" + i, angle3, cubes[i].x, cubes[i].y + cubes[i].offset, cubes[i].radius);
+        }
+        if (angle1 >= 270) {
+
+
+
+
+            alongPath("#circle4" + i, angle4, cubes[i].x, cubes[i].y, cubes[i].radius);
+            alongPath("#circle8" + i, angle4, cubes[i].x, cubes[i].y + cubes[i].offset, cubes[i].radius);
+
+            var a1 = alongPath("#circle" + i, angle1, (cubes[i].x), cubes[i].y, cubes[i].radius);
+            var a2 = alongPath("#circle2" + i, angle2, (cubes[i].x), cubes[i].y, cubes[i].radius);
+            var a3 = alongPath("#circle3" + i, angle3, (cubes[i].x), cubes[i].y, cubes[i].radius);
+            var a4 = alongPath("#circle4" + i, angle4, (cubes[i].x), cubes[i].y, cubes[i].radius);
+            var a5 = alongPath("#circle5" + i, angle1, (cubes[i].x), cubes[i].y + cubes[i].offset, cubes[i].radius);
+            var a6 = alongPath("#circle6" + i, angle2, (cubes[i].x), cubes[i].y + cubes[i].offset, cubes[i].radius);
+            var a7 = alongPath("#circle7" + i, angle3, (cubes[i].x), cubes[i].y + cubes[i].offset, cubes[i].radius);
+            var a8 = alongPath("#circle8" + i, angle4, (cubes[i].x), cubes[i].y + cubes[i].offset, cubes[i].radius);
+            $("#back" + i).attr("points", a4.point + a8.point + a7.point + a3.point).css("display", "block");
+            $("#front" + i).attr("points", a1.point + a5.point + a6.point + a2.point).css("display", "block");
+            $("#bottom" + i).attr("points", a5.point + a6.point + a7.point + a8.point).css("display", "block");
+            $("#left" + i).attr("points", a2.point + a6.point + a7.point + a3.point).css("display", "block");
+            $("#right" + i).attr("points", a1.point + a5.point + a8.point + a4.point).css("display", "block");
+            $("#top" + i).attr("points", a1.point + a2.point + a3.point + a4.point).css("display", "block");
+            $(".polygon" + i).css("z-index", cubesY.y)
+            if (a1.x > a2.x) {
+                changeColor("#front" + i);
+            }
+            if (a4.x < a3.x) {
+                changeColor("#back" + i);
+            }
+            if (a2.x > a3.x) {
+                changeColor("#left" + i);
+            }
+            if (a1.x < a4.x) {
+                changeColor("#right" + i);
+            }
+            changeColor("#bottom" + i);
+        }
+        if (i !== 0) {
+
+            cubes[i].x = alongPath("#circle" + i, angle1-cubes[i].angleOffset, cubes[0].x, cubes[0].y, cubes[0].radius - cubes[i].radiusOffset).x;
+
+            cubes[i].y = (alongPath("#circle" + i, angle1-cubes[i].angleOffset, cubes[0].x, cubes[0].y, (cubes[0].radius - cubes[i].radiusOffset) * 2).y) + ((cubes[0].y / 2) - cubes[i].offset - 1);
+
+        }
+
+    }
+
+
+}
+// $(document).on("click", place);
+
+// function place() {
+//     ang += 5;
+//     // cubes.push({ made: false, x: mouse.x, y: mouse.y, offset: 100, radius: 0 })
+// }
+// function updateMousePos(event) {
+//     mouse.x = event.clientX
+//     mouse.y = event.clientY
+// }
+//visulizor for backround-----------------------------
+
+$(window).on("click", chromeSucks);
+function chromeSucks() {
+if(!clicked){
+    //have to put this in a stupid function on click because google is 'restarted'
+    audio = document.getElementById("audio");
+    audio.src = "./944397.mp3";
+    audio.play();
+    context = new AudioContext();
+    audioSrc = context.createMediaElementSource(audio);
+    analyser = context.createAnalyser();
+    audioSrc.connect(analyser);
+    analyser.connect(context.destination);
+
+    //start of data
+    analyser.fftSize = 256;
+    bufferLength = analyser.frequencyBinCount;
+    clicked = true
+
+}
+}
+function draw() {
+
+    drawVisual = requestAnimationFrame(draw);
+
+    analyser.getByteFrequencyData(dataArray);
+
+}
+
+
