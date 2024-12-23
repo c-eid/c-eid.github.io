7f8d4fb85a61cebdf487c6102f52eb160f3201cf
c-eid <141771146+c-eid@users.noreply.github.com>
2024-12-23 22:22:02 +0000

    Added Sidebar

--- projects/frc website/bots/bots.js
+++ projects/frc website/bots/bots.js
@@ -6,3 +6,13 @@ parallax.onscroll = function () {
     let elBackgrounPos = (parallax.scrollTop * speed) + "px";
     parallax.style.backgroundPositionY = elBackgrounPos;
 };
+
+let portrait = (window.innerWidth / window.innerHeight) < 1;
+if(portrait){
+    document.querySelector("#nav-ul").remove()
+  document.querySelector(".hamburger").style.display = "block"
+}else{
+    document.querySelector(".hamburger").remove()
+    document.querySelector("#nav-ul").style.display = "flex"
+
+}
